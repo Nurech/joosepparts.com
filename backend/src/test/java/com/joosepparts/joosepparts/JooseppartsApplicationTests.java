2836cdcfaabedbaa3f486c844bf33ca9e7479101
Joosep.P <joosep.parts@gmail.com>
2021-03-28 13:54:25 +0300

    a

---
+++ backend/src/test/java/com/joosepparts/joosepparts/JooseppartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joosepparts.joosepparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooseppartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
